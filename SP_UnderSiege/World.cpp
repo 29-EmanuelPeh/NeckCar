b986983e7a9fe59a1033a0fa3adcf8eca2057414
hh <joshng004@gmail.com>
2022-08-23 11:30:09 +0800

    Added legend and allowed player to increase coin after killing guard

--- SP_UnderSiege/World.cpp
+++ SP_UnderSiege/World.cpp
@@ -125,8 +125,16 @@ void World::printWorld(Character* player, bool hostile)
 void World::printWorldMap(Character* player, Shop* shopLocate, bool hostile, Character* guard1, Character* guard2, Character* guard3)
 {
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	for (size_t x = 0; x < 101; x++)
+	std::cout << "-----------------------------" << std::endl;
+	std::cout << "|" << " " << "Legend:" << "                   " << "|" << std::endl;
+	std::cout << "|" << "        ->" <<"(P)layer (YOU)" << "   " << "|" << std::endl;
+	std::cout << "|" << "        ->" << "(G)uards" << "         " << "|" << std::endl;
+	std::cout << "|" << "        ->" << "(S)hop" << "           " << "|" << std::endl;
+	std::cout << "|" << "        ->" << "(C)hest"<< "          " << "|" << std::endl;
+	std::cout << "|" << "        ->" << "(M)iniboss" << "       "<< "|" << std::endl;
+	std::cout << "|" << "        ->" << "(B)oss" << "           " << "|" << std::endl;
+	std::cout << "-----------------------------" << std::endl;
+   	for (size_t x = 0; x < 101; x++)
 	{
 		for (size_t y = 0; y < 101; y++)
 		{
